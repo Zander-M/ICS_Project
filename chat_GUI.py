34d4d2a80a7769a5eba523b9353fce800974a456
Zander_M <sherlingford@yahoo.com>
2018-05-06 01:11:19 +0800

    finished login panel

--- chat_GUI.py
+++ chat_GUI.py
@@ -7,31 +7,56 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
-# from kivy.uix.scrollview import ScrollView
+from kivy.uix.scrollview import ScrollView
+from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 import threading
-from chat_system import commute
 import trans
-# from kivy.uix.dropdown import DropDown
 
+# from kivy.uix.dropdown import DropDown
 # test login
 # Builder.load_file("kv\\command.kv") # test command
 # Builder.load_file("kv\\sonnet.kv") # test sonnet
 # Builder.load_file("kv\\chatting.kv")
+
 class login(Screen):
    
-    def usrlgin(self):
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        self.ids['usrn'].bind(on_text_validate=self.usrlgin)
+
+    def usrlgin(self, *args):
         app = App.get_running_app()
         app.usrn = self.ids['usrn'].text
-        app.cmd('zd', 'command')
+        app.cmd(app.usrn)
+        while trans.system_msg == '':
+            pass
+        if trans.system_msg[0] == 'D':
+            app.notice = 'oops, duplicated name'
+            app.popup()
+        else:
+            app.scrm.current = 'command'
+            app.notice = 'Welcome! ' + app.usrn +'!'
+            app.scrm.current = 'command'
+            app.popup()
 
 class notice(Popup):
     pass
 
 class command(Screen):
-    # logged in stage pane
+    
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+    # logged in stage panel.
+    
+    def on_enter(self, *args):
+        app = App.get_running_app()
+        self.ids['cmd_usrn'].text = "Hello, " + app.usrn + '\nWhat do you want to do?'
+
     def bye(self):
+        # log out of the system
            trans.trans.append('q')
            App.get_running_app().stop()
 
@@ -51,42 +76,34 @@ class sonnet(Screen):
 class mainCanvas(Widget):
     # this is the main canvas handling tasks
     pass
-    # return commandPanel() # test command panel
-    # test login panel
-    # return chattingPanel() # test chatting panel
-       
+    # return commandPanel() 
+
 class Chat_GUI(App):
 
     def __init__(self):
         super().__init__()
         self.usrn = ''
-        self.lk = threading.Lock()
+        self.notice = ''
 
     def build(self):
         Builder.load_file("kv\\chat_system.kv") # load layout files 
         self.scrm = ScreenManager()
         self.scrm.add_widget(login(name='login'))
-        self.scrm.add_widget(command(name='command'))
+        self.scrm.add_widget(command(name='command', id='command'))
         self.scrm.add_widget(chat_with(name='chat_with'))
         self.scrm.add_widget(chatting(name='chatting'))
         self.scrm.add_widget(sonnet(name='sonnet'))
         return self.scrm
     
-    def cmd(self, text, scn):
+    def cmd(self, text):
         trans.trans.append(text)
-        while trans.system_msg == '':
-            pass
-        if trans.local_msg[-1][0]=='D':
-            self.popup(self.scrm.current)
-        else:
-            self.scrm.current = 'command'
         
-    def popup(self, scn):
+    def popup(self, *arg):
         p = notice()
         p.open()
+        self.notice = ''
     
-   
-       # chatThread.start()    
+    # chatThread.start()    
     # def update(self, screen):
     #     if screen == 0:
     #         sm.current('')        
--- chat_system.py
+++ chat_system.py
@@ -5,10 +5,9 @@ import threading
 from chat_client_class import *
 import trans
 
-
-def commute(text):
-    global trans
-    trans = text
+trans.trans = []
+trans.local_msg = []
+trans.system_msg = ''
 
 class ChatSystem:
     
@@ -17,16 +16,6 @@ class ChatSystem:
         self.g_sys = Chat_GUI()
         self.trdlock = threading.Lock()
     
-    def build(self):
-        Builder.load_file("kv\\chat_system.kv") # load layout files 
-        self.scrm = ScreenManager()
-        self.scrm.add_widget(login(name='login'))
-        self.scrm.add_widget(command(name='command'))
-        self.scrm.add_widget(chat_with(name='chat_with'))
-        self.scrm.add_widget(chatting(name='chatting'))
-        self.scrm.add_widget(sonnet(name='sonnet'))
-        return self.scrm
-
     # def init_chat(self):
     #     self.c_sys.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
     #     svr = SERVER # if self.args.d == None else (self.rgs.d, CHAT_PORT)
@@ -48,100 +37,25 @@ class ChatSystem:
         #     self.c_sys.console_input.append(self.trans)
         #     self.trans = ''
 
-    def tst(self):
-        while len(trans.trans) != 0:
-            self.c_sys.console_input.append(trans.trans)
-            trans.trans = [] 
+    # def tst(self):
+    #     while len(trans.trans) != 0:
+    #         self.c_sys.console_input.append(trans.trans)
+    #         trans.trans = [] 
 
     def trdctrl(self): # starting two threads, chat system and GUI
         # self.c_sys.run_chat()
         # self.g_sys.run(
         chatThread = threading.Thread(target=self.run_C_sys)
         appThread = threading.Thread(target=self.run_app)
-        testThread = threading.Thread(target=commute, args=['test'])
+        # testThread = threading.Thread(target=commute, args=['test'])
         appThread.start()
         chatThread.start()
-        testThread.start()
-        testThread.join()
+        # testThread.start()
+        # testThread.join()
         appThread.join()
         chatThread.join()
+
  
 if __name__ == '__main__':
     chat = ChatSystem()
     chat.trdctrl()
-
-# Testing code
-# from chat_client_class import *
-# import threading
-# import kivy
-# from kivy.uix.button import Button
-# from kivy.app import App
-# from kivy.lang import Builder
-# from kivy.uix.label import Label
-# from kivy.uix.scrollview import ScrollView
-# from kivy.uix.gridlayout import GridLayout
-# from kivy.event import EventDispatcher
-
-# # kv = Builder.load_string('''
-# # <Test>
-# #     GridLayout:
-# #         cols:1
-# #         size: root.size
-# #         Button:
-# #             text: 'hello'
-# #             on_press: root.a()
-
-# # ''')
-
-# class Test(Button):
-#     def a(self):
-#         app = App.get_running_app()
-#         app.cmd('zd')
-
-# class ChatSys(App, Client):
-#     def __init__(self):
-#         super().__init__()
-#         self.msg = ''
-    
-#     def build(self):
-#         return Test()
-
-
-#     def init_chat(self):
-#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
-#         svr = SERVER # if self.args.d == None else (self.rgs.d, CHAT_PORT)
-#         self.socket.connect(svr)
-#         self.sm = csm.ClientSM(self.socket)
-#         reading_thread = threading.Thread(target=self.test)
-#         reading_thread.daemon = True
-#         reading_thread.start()
-
-#     def cmd(self, text):
-#         self.console_input.append(text)
-#         return
-
-#     def test1(self):
-#         # login_GUI.ChatSystem().run()
-#         chatThread = threading.Thread(target=self.run_chat())
-#         guiThread = threading.Thread(target=self.run())  
-#         # chatThread.daemon = True
-#         guiThread.start()
-#         chatThread.start()
-                
-#     def test(self):
-#         self.run()
-# def run_client():
-#     client = chat_client_class.Client()
-#     client.run_chat()
-#     cliserver = subprocess.Popen(client.run_chat(), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-
-# def run_GUI():
-#     login_GUI.GUI()
-
-# def test():
-    
-# t1 = threading.Thread(target=test)
-# t2 = threading.Thread(target=run_client)
-# run_client()
-    # t2.start()
-    # t1.start()
